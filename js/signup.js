1b0b6809dff0a3d1fb50884e508d4fd3b6b4b046
dila <137876159+ursystempass@users.noreply.github.com>
2026-03-26 00:35:38 +0700

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -11,7 +11,7 @@ function showSuccessPopup() {
     document.getElementById("popupRegister").classList.add("active");
 
     setTimeout(() => {
-        window.location.href = "/html/sign in.html";
+        window.location.href = "signin.html";
     }, 3000);
 }
 
@@ -28,7 +28,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const existingUser = JSON.parse(localStorage.getItem("smartmom_user"));
 
-        // ❌ VALIDASI (STOP LANGSUNG)
         if (!nama || !email || !password) {
             showErrorPopup("Isi semua data!");
             return;
@@ -44,7 +43,6 @@ document.addEventListener("DOMContentLoaded", function () {
             return;
         }
 
-        // ✅ KALAU LOLOS BARU JALAN KE SINI
         localStorage.setItem("smartmom_user", JSON.stringify({
             nama,
             email,
@@ -57,54 +55,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+feather.replace();
 
-        feather.replace()
+function togglePassword() {
+    const password = document.getElementById("password");
+    const icon = document.getElementById("eyeIcon");
 
-        function togglePassword() {
+    if (password.type === "password") {
+        password.type = "text";
+        icon.setAttribute("data-feather", "eye-off");
+    } else {
+        password.type = "password";
+        icon.setAttribute("data-feather", "eye");
+    }
 
-            const password = document.getElementById("password");
-            const icon = document.getElementById("eyeIcon");
+    feather.replace();
+}
 
-            if (password.type === "password") {
-                password.type = "text";
-                icon.setAttribute("data-feather", "eye-off");
-            } else {
-                password.type = "password";
-                icon.setAttribute("data-feather", "eye");
-            }
-
-            feather.replace();
-        }
-
-
-        const registerForm = document.getElementById("registerForm");
-
-        registerForm.addEventListener("submit", function (e) {
-
-            e.preventDefault();
-
-            const nama = document.getElementById("nama").value;
-            const email = document.getElementById("email").value;
-            const password = document.getElementById("password").value;
-
-            const user = {
-                nama: nama,
-                email: email,
-                password: password
-            };
-
-            localStorage.setItem("smartmom_user", JSON.stringify(user));
-
-            document
-                .getElementById("popupRegister")
-                .classList
-                .add("active");
-
-        });
-
-
-        function goToLogin() {
-
-            window.location.href = "sign in.html";
-
-        }
+function goToLogin() {
+    window.location.href = "signin.html";
+}
